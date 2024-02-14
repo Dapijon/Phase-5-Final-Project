b50bdc6fa498b42fcef4c7c3e641ea2933596a5c
Walter N <2760walter@gmail.com>
2024-02-14 11:31:06 +0300

    initialize server

---
+++ main.py
@@ -0,0 +1,12 @@
+from app import create_app
+from flask import request
+
+app = create_app()
+
+@app.route('/')
+def index():
+    return 'Welcome to Pesa App'
+
+
+if __name__ == '__main__':
+    app.run()
