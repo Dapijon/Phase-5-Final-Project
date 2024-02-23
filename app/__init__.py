9d9e29d0a8d0ca46b6dab9970f1a1648da5b0452
Walter N <2760walter@gmail.com>
2024-02-23 14:12:57 +0300

    add summary routes

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,11 @@ def create_app():
     app = Flask(__name__)
     
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pesa.db'
+    app.config['SECRET_KEY']='klfjkldfjdi13kfdl44j4lkj4'
+    
     db.init_app(app)
+    
+    app.register_blueprint(summary_bp)
     migrate = Migrate(app, db)
     
     
