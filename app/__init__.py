da254972288f18dfdd8c078cddf17de6f10241df
leonkinyanjui <dapijon@gmail.com>
2024-02-23 17:04:18 +0300

    Auth_bp creation and definition

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 from .models import db
+from .auth_bp import auth
 from flask_migrate import Migrate
-
 
 def create_app():
     app = Flask(__name__)
@@ -9,7 +10,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pesa.db'
     db.init_app(app)
     migrate = Migrate(app, db)
-    
-    
-    
+
+    app.register_blueprint(auth, url_prefix='/auth')
+
     return app
