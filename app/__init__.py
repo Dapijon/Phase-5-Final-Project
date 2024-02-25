67e8bf41483b782011dda9a9901513d5630fc85a
leonkinyanjui <dapijon@gmail.com>
2024-02-25 22:50:51 +0300

    Transactions.py formating

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,18 @@ from flask import Flask
 from flask_cors import CORS
 from .models import db
 from .auth_bp import auth
+from .transactions import transactions_bp 
 from flask_migrate import Migrate
 from .summary import summary_bp
 
 def create_app():
     app = Flask(__name__)
-    cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+    cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:3001"}})
     
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pesa.db'
     app.config['SECRET_KEY']='klfjkldfjdi13kfdl44j4lkj4'
+    
+    app.config['JWT_TOKEN_LOCATION'] = ['headers']
     
     db.init_app(app)
     
@@ -18,5 +21,6 @@ def create_app():
     migrate = Migrate(app, db)
 
     app.register_blueprint(auth, url_prefix='/auth')
+    app.register_blueprint(transactions_bp, url_prefix='/transactions')
 
     return app
--- app/transactions.py
+++ app/transactions.py
@@ -1,37 +1,31 @@
 from sqlalchemy.exc import SQLAlchemyError
 from decimal import Decimal
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from datetime import datetime
-from flask import request, redirect, flash, jsonify
 from flask_login import current_user
-from models import db, User, Transaction
+from app.models import db, User, Transaction
 import locale
-import requests
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
-
 transactions_bp = Blueprint('transactions_bp', __name__)
-
 
 @transactions_bp.route('/cash_transfer/<int:receiver_id>/<float:amount>', methods=['POST'])
 @jwt_required()
 def cash_transfer(receiver_id, amount):
-    current_user = get_jwt_identity()
-    sender = current_user
+    sender = User.query.get(get_jwt_identity())
 
     if amount <= 0:
         return jsonify({'error': 'Amount must be greater than zero'})
 
     try:
-        if current_user.balance >= Decimal(amount):
-            current_user.balance -= Decimal(amount)
+        if sender.balance >= Decimal(amount):
+            sender.balance -= Decimal(amount)
             receiver = User.query.get(receiver_id)
             receiver.balance += Decimal(amount)
 
             new_transaction = Transaction(
-                sender=current_user, receiver=receiver, amount=Decimal(amount))
+                sender=sender, receiver=receiver, amount=Decimal(amount))
             db.session.add(new_transaction)
-
             db.session.commit()
 
             return jsonify({'message': 'Transaction successful'})
@@ -41,47 +35,21 @@ def cash_transfer(receiver_id, amount):
         print(f"Error: {str(e)}")
         return jsonify({'error': f"Error: {str(e)}"})
 
-
 @transactions_bp.route('/deposit', methods=['POST'])
 @jwt_required()
 def deposit():
     data = request.get_json()
 
     amount = data.get('amount')
-    memo = data.get('memo')
 
     if not amount or float(amount) <= 0:
         return jsonify({'error': 'Invalid amount'}), 400
 
-    current_user.balance += float(amount)
+    current_user.balance += Decimal(amount)
 
     new_transaction = Transaction(
-        user=current_user, type='deposit', amount=float(amount), memo=memo)
+        sender=current_user, receiver=current_user, amount=Decimal(amount))
     db.session.add(new_transaction)
     db.session.commit()
 
     return jsonify({'message': f'Successful Deposit of {locale.currency(float(amount), grouping=True)} at {datetime.now().strftime("%Y-%m-%d %I:%M %p")}'})
-
-
-@transactions_bp.route('/withdraw', methods=['POST'])
-@jwt_required()
-def withdraw():
-    data = request.get_json()
-
-    amount = data.get('amount')
-    memo = data.get('memo')
-
-    if not amount or float(amount) <= 0:
-        return jsonify({'error': 'Invalid amount'}), 400
-
-    if current_user.balance < float(amount):
-        return jsonify({'error': 'Insufficient balance for withdrawal'})
-
-    current_user.balance -= float(amount)
-
-    new_transaction = Transaction(
-        user=current_user, type='withdraw', amount=float(amount), memo=memo)
-    db.session.add(new_transaction)
-    db.session.commit()
-
-    return jsonify({'message': f'Successful Withdrawal of {locale.currency(float(amount), grouping=True)} at {datetime.now().strftime("%Y-%m-%d %I:%M %p")}'})
