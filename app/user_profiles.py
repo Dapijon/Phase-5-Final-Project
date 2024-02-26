e722b75ec7fdc2bc807d0c48225fe4e98801af0b
kawatech002 <kawanjiku94@gmail.com>
2024-02-26 13:16:00 +0300

    update userprofile.py

--- app/user_profiles.py
+++ app/user_profiles.py
@@ -6,7 +6,14 @@ app = Flask(__name__)
 def update_profile():
     form_data = request.get_json()
     
-   
+    updated_user = {
+        'firstName': form_data['firstName'],
+        'lastName': form_data['lastName'],
+        'email': form_data['email'],
+        'profileImage': form_data['profileImage'],
+    }
+
+    return jsonify(updated_user)
 
 if __name__ == '__main__':
     app.run()
