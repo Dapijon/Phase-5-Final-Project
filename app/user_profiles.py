9a2a9f72019a38613392c46cbba92035c20e3ffc
kawatech002 <kawanjiku94@gmail.com>
2024-02-26 13:14:39 +0300

    update profile.py

--- app/user_profiles.py
+++ app/user_profiles.py
@@ -2,7 +2,11 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
+@app.route('/update_profile', methods=['POST'])
+def update_profile():
+    form_data = request.get_json()
+    
+   
 
 if __name__ == '__main__':
     app.run()
